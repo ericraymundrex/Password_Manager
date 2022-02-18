06a18b68fd5f8791df0a537459ed2e4f989c9190
EricRex <ericrexs8@gmail.com>
2022-02-18 12:35:00 +0530

    init

--- app.sh
+++ app.sh
@@ -1,12 +1,2 @@
-# USER PERMISSIONS:
-# chmod -x app.sh - before we run the code
 
-# PACKAGES NEEDED:
-# xdotool
-
-mousepad&
-sleep 2
-xdotool type "Hello this is Eric"
-xdotool key KP_Enter
-xdotool type "The entered key is pressed."
-sleep2
+#!/bin/bash/

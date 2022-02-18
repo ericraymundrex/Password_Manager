b04d8469fda70a9ee878b97461cb264b76da8bde
EricRex <ericrexs8@gmail.com>
2022-02-18 11:19:55 +0530

    This is the basic code

---
+++ README.md
@@ -0,0 +1,2 @@
+Packages needed:
+- xdotool
---
+++ app.sh
@@ -0,0 +1,14 @@
+# USER PERMISSIONS:
+# chmod -x app.sh - before we run the code
+
+# PACKAGES NEEDED:
+# xdotool
+
+# NOTES:
+# mousepad -> When there is change in the mouse.
+# &        -> When we don't want the system to be hanged.
+# sleep    -> When there is we want the system to wait.
+
+mousepad&
+sleep 2
+xdotool type "Hello this is Eric"

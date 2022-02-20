fe5355a92d43c02ed3b3e0861af9216e886df163
EricRex <ericrexs8@gmail.com>
2022-02-20 13:42:22 +0530

    I added the authetication feature and all the users can loged in and sign up

--- app.sh
+++ app.sh
@@ -1,73 +1,115 @@
 #!/bin/sh
 
-a=1
-one=1
-while [ $a -ne 0 ]
-do
+AUTH_STRING="This_is_the_assignment_given_by_Mayur"
+Token=''
 
-    # Code to add or select the user
-    echo "Enter the operation:\n1. Enter a new account\n---------\n Choose a User :"
-    index=1
-    while read p; do
-        echo "$((index+1)) $p"
-        index=$((index+1))
-    done <user.txt
-    if [ $index = 2 ]
+# --------------------------------------------------------------------------------
+# To create a SHA256 hash
+# 1    -> Parameter
+# Hash -> The hash code
+hash=''
+sha256_function () {
+    hash=$(echo $1  | sha256 -s $AUTH_STRING | base64)
+}
+# --------------------------------------------------------------------------------
+# To Generate the Unique ID with the user name and fingerprint for the password
+# Don't use this function other than sign in
+fingerprint=''
+unique_id=''
+Sign_Up () {
+    sha256_function $1
+    fingerprint=$hash
+
+    sha256_function $2
+    unique_id=$hash
+}
+# --------------------------------------------------------------------------------
+# 1 -> User Name
+# 2 -> Password
+# Token -> Has the User Information as the data
+Auth () {
+
+    #Convert the current password to hash
+    sha256_function $2
+    password=$hash
+
+    #Search the data
+    user_data=$(grep -i "$1" User.json)
+
+    #Making the parameter to the correct formate
+    user_quote=$(echo '"'$1'"')
+    pass_quote=$(echo '"'$password'"')
+
+    #Useing JQ to get the needed data from the User.json
+    user_auth=$(echo $user_data | jq '.USER')
+    pass_auth=$(echo $user_data | jq '.FINGERPRINT')
+
+    #Authenticating the user
+    if [ "$user_quote" = "$user_auth" ]
     then
-        echo "There is no users"
-    fi
-    index=2
-
-    # Option is used to map to new user or current users in the user.txt
-    read option
-
-    # Option 1 is to choose new user
-    if [ $option = 1 ]
+        if [ "$pass_quote" = "$pass_auth" ]
         then
-            echo "Enter your name : "
-            read data
-            echo $data>> user.txt
-
-            echo "Enter your password : "
-            read data
-            echo $data| openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt -pass pass:'pick.your.password'>>pass.txt
-    else
-        # option-1 = line number in the user.txt
-        # Finding the user
-        echo "Enter the password "
-        read password
-        index=1
-        while read p; do
-            if [ $option = $index ]
-                then
-                  echo $p | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -pass pass:'pick.your.password' > temp.txt
-                while read p; do
-                    PASS=$p
-                done <temp.txt
-                else
-                    index=$((index+1))
-                    echo "">> temp.txt
-            fi
-        done <pass.txt
-        
-        if [ $PASS = $password ]
-        then
-            echo "1. Add new account\n2. View Password"
-            read option_sub_1
-            if [ $option_sub_1 = 1 ]
-            then
-                echo "Enter the email ID :"
-                read email
-                echo "Enter the password :"
-                read pass
-                echo "$(($option-1)) $email $pass" >>account.txt
-                
-            elif [ $option_sub_1 = 2 ]
-            then
-                grep -i "$(($option-1))" account.txt
-            fi
+            echo "Login Success"
+            Token=$(echo "$user_data")
+        else
+            Token=''
+            echo "Password is wrong"
         fi
+    else
+        Token=''
+        echo "No user Exist"
     fi
-    echo "Press 0 +ENTER to exit"
-   read a
+}
+
+# --------------------------------------------------------------------------------
+# -----------------------------------MAIN CODE------------------------------------
+ending=1
+while [ $ending -ne 0 ]
+do
+    clear
+    echo "1. Sign Up\n2. Sign In"
+
+    # Reading an option
+    read option_user
+
+    if [ $option_user -eq 1 ]
+    then
+        clear
+        echo "User Name : "
+        read user
+        echo "Password : "
+        read pass
+        exits=$(grep -i "$user" User.json)
+        user_exits=$(echo $exits | jq '.USER')
+        if [ -z $user_exits ]
+        then
+            Sign_Up $pass $user
+            #------
+            echo '{"ID" : "'$unique_id'" ,"USER" : "'$user'" ,"FINGERPRINT" : "'$fingerprint'"}'>>User.json
+            echo "SUCCESSFULLY SIGNED - UP"
+        else
+            
+            echo "Account alread exists"
+        fi
+    elif [ $option_user -eq 2 ]
+    then
+        if [ -z $Token ]
+        then
+            clear
+            echo "User Name : "
+            read user
+            echo "Password : "
+            read pass
+            Auth $user $pass
+            echo $Token
+        else
+            echo "Already logged in"
+        fi
+    else    
+        clear
+        echo "Enter a valid option"
+    fi
+
+    echo "PRESS ANY NUMBER TO MAIN PAGE.\nEnter 0+ENTER to EXIT"
+    read ending
 done
